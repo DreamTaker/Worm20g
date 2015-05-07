14545e486b036c7df4243141f02b65fc10a4ebf0
DreamTaker <laststand54@hotmail.com>
2015-05-07 21:02:14 +0930

    Added projects for distributed transaction

---
+++ DataAccess/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Transactions;
+
+namespace DataAccess
+{
+    public class ADODTS
+    {
+
+        /// <summary>
+        /// Demo of Distributed Transactions
+        /// </summary>
+        /// <param name="conn1"></param>
+        /// <param name="com1"></param>
+        /// <param name="conn2"></param>
+        /// <param name="com2"></param>
+        /// <remarks>uses System.Transactions
+        /// Ensure Windows Service - Distributed Transaction Coordinator - is running.
+        /// </remarks>
+        public static String PerformDistTrans(String connString1,String commandText1,String connString2,String commandText2)
+        {
+            string retVals = string.Empty;
+
+            try
+            {
+                using(TransactionScope ts = new TransactionScope())
+                {
+                    using(SqlConnection conn1 = new SqlConnection(connString1))
+                    {
+                        SqlCommand command1 = new SqlCommand(commandText1,conn1);
+                        conn1.Open();
+                        var retVal = command1.ExecuteNonQuery();
+
+                        using(SqlConnection conn2 = new SqlConnection(connString2))
+                        {
+                            SqlCommand command2 = new SqlCommand(commandText2,conn2);
+                            conn2.Open();
+                            retVal = command2.ExecuteNonQuery();
+                        }
+
+                    }
+
+                    ts.Complete();
+                    retVals = "Complete";;
+                }
+            }
+            catch(Exception ex)
+            {
+                retVals = ex.Message;
+            }
+            finally
+            {
+
+            }
+
+            return retVals;
+        }
+
+    }
+
+}
---
+++ UIConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+
+           string ret = ADODTS.PerformDistTrans(@"Data Source=(localdb)\ProjectsV12;Initial Catalog=DB12;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False"
+                ,"INSERT INTO [dbo].[Table] (Id,Text12) VALUES (1,'T12-1')"
+                ,@"Data Source=ORION\DEVINSTANCE;Initial Catalog=DBDI;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"
+                ,"INSERT INTO [dbo].[Table] (DBDIText) VALUES ('DBDI-1')");
+
+            Console.WriteLine(ret);
+
+            while(true)
+            { }
+        }
+    }
+}
